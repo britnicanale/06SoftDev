ff9b10c5f7f2b7364f52ba21925038a4dbc7abca
Britni <bcanale@stuy.edu>
2018-09-22 21:02:43 -0400

    Fixed random output of occupation, added header and footer to table. Things to improve: robustness, simplicity, clarity of text

--- 10_my_foist_template/app.py
+++ 10_my_foist_template/app.py
@@ -13,16 +13,19 @@ app = Flask(__name__)
 
 occs = {}
 
+
 def getOcc(filename):
-    f = open(filename, 'r') #open csv                                                      
-    lineSeparated = csv.reader(f) #separate by commas, but not those in quotes             
+    f = open(filename, 'r')                                                      
+    lineSeparated = csv.reader(f) 
     for line in lineSeparated:
-        if line[0] != 'Job Class': #control for leading line                               
-            if line[0] != 'Total': #control for last line
-                titleKey = line[0]
-                print(titleKey)
-                percentValue = float(line[1]) #save percent value as floating point        
-                occs[titleKey] = percentValue
+        if line[0] == 'Job Class': #exception for first, doesn't store value as float, used for heading of table
+            titleKey = line[0]
+            val = line[1]
+            occs[titleKey] = val
+        if line[0] != 'Job Class':#includes total to be used for end of table
+            titleKey = line[0]
+            percentValue = float(line[1])
+            occs[titleKey] = percentValue
 
 @app.route("/")
 def hello_world():
@@ -30,8 +33,9 @@ def hello_world():
 
 @app.route("/my_foist_template")
 def hello_temp():
-    getOcc("static/occupations.csv")
-    return render_template("temp.html", ttl = "Random Stuff", rand = random.choice(list(occs.keys())), dict = occs)#len = len(keys), k = keys, v = vals)
+    getOcc("static/occupations.csv") #calls function that created dictionary out of the csv file
+    return render_template("temp.html", ttl = "Random Stuff", rand = list(occs.keys())[random.randint(1, len(list(occs.keys())) - 2)], dict = occs)
+#                          filename     title of page         random occupation                                                        dictionary of occupations
 
 
 if __name__ == "__main__":
