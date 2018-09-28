7032ada0fc49bf03dd7d9da678f5d483541c32c1
Britni <bcanale@stuy.edu>
2018-09-27 21:18:14 -0400

    Finished app, commented, added more info

--- 13_information/app.py
+++ 13_information/app.py
@@ -7,20 +7,20 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/")                         #home route
 def hello_world():
-    print(app)
-    return render_template("form.html")
+    print(app)                          #info for debugging/informational purposes
+    return render_template("form.html") #renders template for form, which has fields for names and submit button
 
 
-@app.route("/auth")
-def authenticate():
-    print(app)
+@app.route("/auth")                     #route for echo
+def authenticate():                     #enders echo template, uses request to get info from forms and request type, which is put into echo temp
+    print(app)                          #info for debugging/informational purposes
     print(request)
     print(request.args)
-    return render_template("echo.html", req = request.method, first = request.args["fname"], last = request.args["lname"])
+    return render_template("echo.html", req = request.method, first = request.args["fname"], last = request.args["lname"]) 
 
 
-if __name__ == "__main__":
+if __name__ == "__main__":              #runs app
     app.debug = True
     app.run()
