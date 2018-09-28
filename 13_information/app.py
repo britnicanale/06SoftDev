5fc7452250b763e49e06aaa6aa499919ffc6fadd
Britni <bcanale@stuy.edu>
2018-09-27 21:00:20 -0400

    working app, commenting for clarity and organizing work

--- 13_information/app.py
+++ 13_information/app.py
@@ -1,7 +1,7 @@
 #Britni Canale
 #SoftDev1 pd 6
-#K -- 
-#
+#K13 -- Echo Echo Echo
+#2018-09-27
 
 from flask import Flask, render_template, request
 
@@ -10,7 +10,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     print(app)
-    return render_template("temp.html")
+    return render_template("form.html")
 
 
 @app.route("/auth")
@@ -18,7 +18,7 @@ def authenticate():
     print(app)
     print(request)
     print(request.args)
-    return "Waaaa hooo HAAAH"
+    return render_template("echo.html", req = request.method, first = request.args["fname"], last = request.args["lname"])
 
 
 if __name__ == "__main__":
