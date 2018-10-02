cc64d8b2f7b84ef697dd283954c1370b130b91cb
Britni <bcanale@stuy.edu>
2018-10-01 20:57:58 -0400

    integrating with kyle

--- 14_login/app.py
+++ 14_login/app.py
@@ -1,32 +1,44 @@
-#Britni Canale
+#Britni Canale & Kyle Tau
 #SoftDev1 pd 6
-#K -- 
-#
+#K14 -- Do I Know You?
+#2018-10-01
 
 from flask import Flask, render_template, session, request, url_for, redirect
 import os
 
-str = os.urandom(32)
 app = Flask(__name__)
 
-app.secret_key = str
-session["bcanale"] = "hi"
+
+app.secret_key = os.urandom(32)               #generates random 32-bit secret_key
+
+    
+uname = "tom"                                 #HARDCODED USERNAME AND PASSWORD       
+pword = "jerry"
 
 
-@app.route("/")
-def hello_world():
-    if request.form["uname"] in session:
-        return redirect(url_for("welcome"))
-    return render_template("login.html")
-
+@app.route("/")                               #root route, redirects to welcome if logged in, loads login page if not
+def login():
+    if "username" not in session:
+        return render_template("login.html")
+    return redirect(url_for("welcome"))
 
 @app.route("/welcome")
-@app.route("/welcome", methods=["POST", "GET"])
+@app.route("/welcome", methods=["POST"])      #welcome route (if logged in only), only accepts POST requests
 def welcome():
-    print(request.args)
-    print(request.method)
-    if request.form["uname"] in session and request.form["pword"] == session[request.form["uname"]]:
-        return render_template("welcome.html", uname = request.form["uname"])
+    un = request.form["uname"]                #gets login info from form
+    pw = request.form["pword"]
+    if un == uname and pw == pword:           #checks username and password, if correct adds to session and renders welcome page
+        session["username"] = uname
+        return render_template("welcome.html")
+    return render_template("error.html")      #displays error messages if something went wrong, has button to go back to login page
+
+
+@app.route("/logout")                         #route for logging out
+def go():
+    if 'username' in session:                 #ends session
+        session.clear()
+    return redirect(url_for("login"))         #redirects to login page after logout
+
 
 if __name__ == "__main__":
     app.debug = True
