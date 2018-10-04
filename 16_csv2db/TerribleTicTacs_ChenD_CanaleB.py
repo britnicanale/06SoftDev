a97f9fddddbbd90d723c75232c22041a9dd77ecc
Britni <bcanale@stuy.edu>
2018-10-04 19:55:05 -0400

    Fixed column names, added comments

--- 16_csv2db/TerribleTicTacs_ChenD_CanaleB.py
+++ 16_csv2db/TerribleTicTacs_ChenD_CanaleB.py
@@ -18,13 +18,19 @@ c = db.cursor()                           #allows sqlite to be used on database.
 
 def makeTable(filename):
     with open(filename, 'r') as csvfile:  #opens database
-        reader = csv.DictReader(csvfile)  #creates sequence of dictionaries, creates a table
-        c.execute("CREATE TABLE " + filename[0:-4] + "(code TEXT, mark INTEGER, id INTEGER)")
-        for row in reader:                #goes through each row, creates variables for columns depending on file input    
-            col1 = list(row.keys())[0] 
-            col2 = list(row.keys())[1]
-            col3 = list(row.keys())[2]
-            params = (row[col1],row[col2],row[col3])
+        reader = csv.DictReader(csvfile)  #creates sequence of dictionaries
+        num = 0                           #used to determine if row is first row
+        col1 = ""                         #initializes column strings
+        col2 = ""
+        col3 = ""
+        for row in reader:                #goes through each row, adds to table
+            if num == 0:                  #if row is first row, names columns based on csv file, uses list of keys to access header names
+                col1 = list(row.keys())[0] 
+                col2 = list(row.keys())[1]
+                col3 = list(row.keys())[2]
+                c.execute("CREATE TABLE " + filename[0:-4] + "(" +col1+ "  TEXT, " + col2+ " INTEGER, " +col3+" INTEGER)")  #creates table
+                num = num + 1             #increments to indicate first row has been passed
+            params = (row[col1],row[col2],row[col3])                                 #creates params for values using row dictionary and column names  
             c.execute("INSERT INTO " + filename[0:-4] + " VALUES(?, ?, ?)", params)  #inserts values in each row into the table
 
 
