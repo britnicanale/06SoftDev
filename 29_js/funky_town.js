4d0b68ad0d58559a020843d7e38fca9c414bda3d
Britni <bcanale@stuy.edu>
2018-12-19 21:52:36 -0500

    Fixed errors

--- 29_js/funky_town.js
+++ 29_js/funky_town.js
@@ -5,6 +5,8 @@
  2018-12-19
 */
 
+
+//functions
 var fibonacci = function(n) {
     if(n <2){
       return n;
@@ -21,10 +23,40 @@ var gcd = function(a,b){
     else if (a > b){
       return gcd(b,a-b);
     }
-    return gcd(a,b-a);
+    else{
+      return gcd(a,b-a);
+    }
 };
 
 var students = ['Jeffrey','Britni','Thomas', 'Tim','Damian'];
 var randomStudent = function(){
     return students[Math.floor(Math.random()*students.length)]
 }
+
+
+
+//RETRIEVERS
+
+//gcd
+var gcdClick = document.getElementById("gcd");
+var getGCD = function(){
+  console.log(gcd(Math.floor(Math.random()*30), Math.floor(Math.random()*30)));
+  document.getElementById("gcd");
+};
+gcdClick.addEventListener('click', getGCD);
+
+//fib
+var fibClick = document.getElementById("fib");
+var getFib = function(){
+  console.log(fibonacci(Math.floor(Math.random()*15)));
+  document.getElementById("fib");
+};
+fibClick.addEventListener('click', getFib);
+
+//random student
+var randClick = document.getElementById("randStu");
+var getRand = function(){
+  console.log(randomStudent());
+  document.getElementById("randStu");
+};
+randClick.addEventListener('click', getRand);
